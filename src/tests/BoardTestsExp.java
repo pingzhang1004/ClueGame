ff5afb5c24f9f845014b41faef6844fd55475797
Yonghao Li <yonghaoli@mines.edu>
2022-10-06 17:59:17 -0600

    Add all test

--- src/tests/BoardTestsExp.java
+++ src/tests/BoardTestsExp.java
@@ -23,118 +23,172 @@ public class BoardTestsExp {
 
 
 	//test the creation of adjacency lists for a 4x4 board, including:
+	
 	@Test
-	public void testAdjacency() {
-
-		//test the creation of adjacency lists for the top left corner (i.e., location [0][0])
+	public void testAdjacency_1() {
+		// test the creation of adjacency lists for the top left corner --- [0,0]
 		TestBoardCell cell = board.getCell(0, 0);
-		Set<TestBoardCell>  testList = cell.getAdjList();
+		Set<TestBoardCell> testList = cell.getAdjList();
 		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
 		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
 		Assert.assertEquals(2, testList.size());
-
-		//test the creation of adjacency lists for bottom right corner (i.e., location [3][3])
-		cell = board.getCell(3, 3);
-		testList = cell.getAdjList();
+	}	
+		
+	@Test
+	public void testAdjacency_2() {
+		// test the creation of adjacency lists for bottom right corner --- [3,3]
+		TestBoardCell cell = board.getCell(3, 3);
+		Set<TestBoardCell> testList = cell.getAdjList();
 		Assert.assertTrue(testList.contains(board.getCell(2, 3)));
 		Assert.assertTrue(testList.contains(board.getCell(3, 2)));
 		Assert.assertEquals(2, testList.size());
-
-		//test the creation of adjacency lists for a right edge (e.g., location [1][3])
-		cell = board.getCell(1, 3);
-		testList = cell.getAdjList();
+	}
+	@Test
+	public void testAdjacency_3() {
+		// test the creation of adjacency lists for a right edge --- [1,3]
+		TestBoardCell cell = board.getCell(1, 3);
+		Set<TestBoardCell> testList = cell.getAdjList();
 		Assert.assertTrue(testList.contains(board.getCell(0, 3)));
 		Assert.assertTrue(testList.contains(board.getCell(1, 2)));
 		Assert.assertTrue(testList.contains(board.getCell(2, 3)));
 		Assert.assertEquals(3, testList.size());
-
-		///test the creation of adjacency lists for a left edge (e.g., location [3][0])
-		cell = board.getCell(1, 3);
-		testList = cell.getAdjList();
-		Assert.assertTrue(testList.contains(board.getCell(2, 0)));
-		Assert.assertTrue(testList.contains(board.getCell(3, 1)));
-		Assert.assertEquals(2, testList.size());
-
 	}
-
-
-	//	Methods (minimum 5) to test target creation on a 4x4 board (see examples below)
 	
-	
-	//Test for behavior on empty 4x4 board.?????
-	
-	
-	//Test for behavior with at least one cell being flagged as occupied.  A player cannot move into an occupied cell.
 	@Test
-	public void testCellOccupied() {
-		//set up occupied cells.
-		board.getCell(0, 2).setOccupied(true);
-		
-		TestBoardCell cell = board.getCell(0, 0);
-		board.calcTargets(cell, 2);
-		Set<TestBoardCell>  targets = board.getTargets();
-		Assert.assertEquals(2, targets.size());
-		Assert.assertTrue(targets.contains(board.getCell(1, 1)));
-		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
-		
-		//Assert.assertFalse(targets.contains(board.getCell(0, 2)));		
-		
+	public void testAdjacency_4() {
+		// test the creation of adjacency lists for a middle of grid --- [1,1]
+		TestBoardCell cell = board.getCell(1, 1);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(0, 1)));
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(1, 2)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 1)));
+		Assert.assertEquals(4, testList.size());
 	}
 	
+	@Test
+	public void testAdjacency_5() {
+		// test the creation of adjacency lists for a left edge --- [2,0]
+		TestBoardCell cell = board.getCell(2, 0);
+		Set<TestBoardCell> testList = cell.getAdjList();
+		Assert.assertTrue(testList.contains(board.getCell(1, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(3, 0)));
+		Assert.assertTrue(testList.contains(board.getCell(2, 1)));
+		Assert.assertEquals(3, testList.size());
 
-	//Test for behavior with at least one cell being flagged as a room. A player used up all movement points upon entering a room.
-	public void testCellFlaggedRoom() {
-		//set up flagged room.
-		board.getCell(0, 3).setIsRoom(true);
-		
-		TestBoardCell cell = board.getCell(0, 0);
+	}
+	
+	
+    // Methods to test target creation on a 4x4 board
+	
+	// Test for behavior on empty 4x4 board.
+	@Test
+    public void testTargetsEmpty_1() {
+		TestBoardCell cell = board.getCell(3, 1);
 		board.calcTargets(cell, 3);
-		Set<TestBoardCell>  targets = board.getTargets();
-		Assert.assertEquals(5, targets.size());
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(7, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 2)));
+	}
+	
+	@Test
+    public void testTargetsEmpty_2() {
+		// Test for the maximum die roll
+		TestBoardCell cell = board.getCell(1, 2);
+		board.calcTargets(cell, 6);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(7, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
 		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 2)));
+	}
+
+	// Test for behavior with at least one cell being flagged as occupied.
+	// Target test with occupied --- [2,2]
+	@Test
+	public void testTargetsOccupied_1() {
+		board.getCell(2, 2).setOccupied(true);
+		TestBoardCell cell = board.getCell(2, 1);
+		board.calcTargets(cell, 2);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(5, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
 		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
 		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
-		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
-		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
-		
-		//used up all movement points upon entering a room
-		//Assert.assertFalse(targets.contains(board.getCell(0, 3)));		
-		
+		Assert.assertTrue(targets.contains(board.getCell(3, 2)));
 	}
-	
-	
-	
-	
+
+	// Target test with occupied --- [2,2] and [1,0]
 	@Test
-	public void testTargetsNormal() {
-		TestBoardCell cell = board.getCell(0, 0);
-		board.calcTargets(cell, 3);
-		Set<TestBoardCell>  targets = board.getTargets();
-		Assert.assertEquals(6, targets.size());
-		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
-		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+	public void testTargetsOccupied_2() {
+		board.getCell(2, 2).setOccupied(true);
+		board.getCell(1, 0).setOccupied(true);
+		TestBoardCell cell = board.getCell(2, 1);
+		board.calcTargets(cell, 2);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(5, targets.size());
 		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
 		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 2)));
+	}
+	
+	// Test for behavior with at least one cell being flagged as a room. 
+	// Target test with rooms -- [1,2]
+	@Test
+	public void testTargetsIsRoom_1() {
+		board.getCell(1, 2).setIsRoom(true);
+		TestBoardCell cell = board.getCell(2, 2);
+		board.calcTargets(cell, 1);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 3)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 2)));
+	}
+	
+	// Target test with rooms -- [3,3] and [1,2]
+	@Test
+	public void testTargetsIsRoom_2() {
+		board.getCell(3, 3).setIsRoom(true);
+		board.getCell(1, 2).setIsRoom(true);
+		TestBoardCell cell = board.getCell(2, 2);
+		board.calcTargets(cell, 3);
+		Set<TestBoardCell> targets = board.getTargets();
+		Assert.assertEquals(7, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 1)));
 		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
 		Assert.assertTrue(targets.contains(board.getCell(1, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
+		Assert.assertTrue(targets.contains(board.getCell(2, 1)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 0)));
+		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
 	}
-
-	// Target test with rooms and occupied
+	
+	// Target test with rooms -- [3,2] and occupied --- [2,1]
 	@Test
 	public void testTargetsMixed() {
 		
-		//set up occupied cells.
-		board.getCell(0, 2).setOccupied(true);
-		board.getCell(1, 2).setIsRoom(true);
-		
-		TestBoardCell cell = board.getCell(0, 3);
+		board.getCell(2, 1).setOccupied(true);
+		board.getCell(3, 2).setIsRoom(true);
+		TestBoardCell cell = board.getCell(3, 3);
 		board.calcTargets(cell, 3);
-		Set<TestBoardCell>  targets = board.getTargets();
+		Set<TestBoardCell> targets = board.getTargets();
 		Assert.assertEquals(3, targets.size());
+		Assert.assertTrue(targets.contains(board.getCell(0, 3)));
 		Assert.assertTrue(targets.contains(board.getCell(1, 2)));
-		Assert.assertTrue(targets.contains(board.getCell(2, 2)));
-		Assert.assertTrue(targets.contains(board.getCell(3, 3)));
-		
+		Assert.assertTrue(targets.contains(board.getCell(3, 2)));
 	}
 	
 
