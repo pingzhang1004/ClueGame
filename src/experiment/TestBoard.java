282cab3727df09ab090a391d832e375098cc7fc8
Yonghao Li <yonghaoli@mines.edu>
2022-10-06 18:08:12 -0600

    Final check for testing

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -14,29 +14,22 @@ public class TestBoard {
 		
 	//A constructor that sets up the board
 	public TestBoard() {
-		super();
 		TargetsList = new HashSet<TestBoardCell>();
-		
 	}
 
 	//calculates legal targets for a move from startCell of length pathlength.
 	//leave the method blank (if void)
-	public void calcTargets( TestBoardCell startCell, int pathlength) 
-	{
-	
+	public void calcTargets( TestBoardCell startCell, int pathlength) {
 	}
 	
 	//  gets the targets last created by calcTargets()
 	//return an empty object (example, an empty map instead of a null) 
 	public Set<TestBoardCell> getTargets(){		
-		
-		return TargetsList;
-		
+		return TargetsList;	
 	}
 	
 	//returns the cell from the board at row, col.
 	public TestBoardCell getCell( int row, int col ) {
-		
 		cell = new TestBoardCell(row, col);
 		return  cell;
 	}
--- src/experiment/TestBoardCell.java
+++ src/experiment/TestBoardCell.java
@@ -5,9 +5,7 @@ import java.util.Set;
 
 //Class TestBoardCell (in experiment package)- represents one cell in your grid
 public class TestBoardCell {
-	
-	//private int row;
-	//private int column;
+
 	private Set<TestBoardCell> adjacencyList;
 	
     //A constructor that has passed into it the row and column for that cell.
@@ -18,32 +16,29 @@ public class TestBoardCell {
 	
     //A setter to add a cell to this cells adjacency list
 	public void addAdjacency( TestBoardCell cell ) {
-	
 	}
 	
     //returns the adjacency list for the cell
-	public Set<TestBoardCell> getAdjList(){
-		
+	public Set<TestBoardCell> getAdjList(){	
 		return adjacencyList;		
 	}
 	
-	
-	//用第二种方法不是第一种方法，设置一个getter 和setter？？？？？？
-	
-    //for indicating a cell is part of a room (void setRoom(boolean) and perhaps boolean isRoom()).	
+    //for setting a cell is part of a room 
 	public void setIsRoom(boolean isRoom)
-	{
-	     
+	{   
 	}
 	
-	//for seting a cell is occupied
+	//for indicating a cell is part of a room 
+	public boolean getIsRoom() {
+	     return true;
+	}	
+	
+	//for setting a cell is occupied
 	public void setOccupied(boolean isOccupied) {
-		
 	}
 	
     //for indicating a cell is occupied by another player 
 	public boolean getOccupied() {
-		
 		return true;
 	}
 	
