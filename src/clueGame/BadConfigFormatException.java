97e2a905bd446da0daaf17cebbc72a7e882e3c29
Yonghao Li <yonghaoli@mines.edu>
2022-10-09 19:24:38 -0600

    Finish BadConfigFormatException.java for testing

--- src/clueGame/BadConfigFormatException.java
+++ src/clueGame/BadConfigFormatException.java
@@ -1,6 +1,6 @@
 package clueGame;
 
-
+// An exception (extends Exception) to indicate data files have bad format
 public class BadConfigFormatException extends Exception {
 
 	// default constructor
