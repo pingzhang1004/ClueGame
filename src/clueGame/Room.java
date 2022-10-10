d647fe81dd28ffda113c847739439a2ff4a4175c
Yonghao Li <yonghaoli@mines.edu>
2022-10-09 19:20:35 -0600

    Finish Room.java for testing

--- src/clueGame/Room.java
+++ src/clueGame/Room.java
@@ -4,24 +4,28 @@ import experiment.TestBoardCell;
 
 public class Room {
 
+	// Variables
 	private String name;
 	private BoardCell centerCell;
 	private BoardCell lableCell;
 	private BoardCell cell;
 	
+	// default constructor
 	public Room() {
 		cell = new BoardCell();
 	}
 	
-	
+	// return a room name
 	public String getName() {
 		return name;
 	}
 	
+	// return a label cell
 	public BoardCell getLabelCell() {
 		return cell;
 	}
 	
+	// return a center cell
 	public BoardCell getCenterCell() {
 		return cell;
 	}
