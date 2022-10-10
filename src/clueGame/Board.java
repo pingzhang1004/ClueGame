24cd81f0042ab651b0fab25712c29332fa719284
Yonghao Li <yonghaoli@mines.edu>
2022-10-09 19:13:25 -0600

    Finish Board.java for testing

--- src/clueGame/Board.java
+++ src/clueGame/Board.java
@@ -5,8 +5,9 @@ import java.util.Map;
 import experiment.TestBoardCell;
 
 public class Board {
+	
+	// Variables
 	private TestBoardCell[][] grid;
-
 	private int numRows;
 	private int numColumns;
 	private String layoutConfigFile;
@@ -15,48 +16,55 @@ public class Board {
 	private Room room = new Room();
 	private BoardCell cell;
 	
+	// Start from Canvas
 	/*
 	 * variable and methods used for singleton pattern
 	 */
 	private static Board theInstance = new Board();
-
 	// constructor is private to ensure only one can be created
 	private Board() {
 		super() ;
 	}
-
 	// this method returns the only Board
 	public static Board getInstance() {
 		return theInstance;
 	}
-
 	/*
 	 * initialize the board (since we are using singleton pattern)
 	 */
 	public void initialize()
 	{
 	}
+	// End from Canvas
 	
+	// Load setup file
 	public void loadSetupConfig() {	
 	}
 	
+	// Load layout file
 	public void loadLayoutConfig() {
 	}
 	
-	public void setConfigFiles(String layout, String setup) {
-		
+	// Set all config files
+	public void setConfigFiles(String layout, String setup) {	
 	}
 	
+	// return a room object by passing a char
 	public Room getRoom(char letter) {
 		return room;
 	}
+	
+	// return a room object by passing a BoardCell
 	public Room getRoom(BoardCell cell) {
 		return room;
 	}
 	
+	// return the number of rows
 	public int getNumRows() {	
 		return 0;
 	}
+	
+	// return the number of columns
 	public int getNumColumns() {	
 		return 0;
 	}
